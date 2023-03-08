86aaab253d704107bfc417371c7ef7ee5523b355
Dera <chideronyekachi04@gmail.com>
2023-03-07 18:21:31 -0800

    edited 11-pow.py file

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def power(a,b):
-	if b==0:
-		return 1
-	elif a==0:
-		return 0
-	elif b==1:
-		return a
-	else:
-		return a*power(a,b-1)
+    res=1
+    for i in range(b):
+        res *= a
+    return res
