3181ecd3572133ec444ff1b6647abfe5adf26342
Dera <chideronyekachi04@gmail.com>
2023-03-11 20:09:51 -0800

    fixed issues in 4-new_in_list.py file

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-    else:
-        nw_list = my_list.copy()
-        nw_list[idx] = element
-        return nw_list
+    if my_list:
+        if idx < 0 or idx >= len(my_list):
+            return my_list
+        else:
+            nw_list = my_list.copy()
+            nw_list[idx] = element
+            return nw_list
