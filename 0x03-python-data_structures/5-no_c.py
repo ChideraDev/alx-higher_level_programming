d119811bbd1d806b41aa15bdee229d782db1efe0
Dera <chideronyekachi04@gmail.com>
2023-03-12 16:27:53 -0700

    fixed issues in 5-no_c.py file

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    my_new_string = my_string.translate({ord(i):None for i in 'cC'})
+    my_new_string = my_string.translate({ord("c"):None})
+    my_new_string = my_new_string.translate({ord("C"):None})
     return my_new_string
