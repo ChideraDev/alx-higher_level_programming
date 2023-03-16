f5be4864602765a4a1913b9e8e198e9490c4474f
Dera <chideronyekachi04@gmail.com>
2023-03-15 17:52:55 -0700

    created 10-best_score.py file

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    mutiplied_result = list(map(lambda item: item * number, my_list))
-    return mutiplied_result
+    return (list(map(lambda item: item * number, my_list)))
